d1f36a20391552ea7d65179f4409da0bcd5bf76e
H26 <josephh2601@gmail.com>
2020-07-07 20:13:57 +0100

    Changed excersises made more efficient

--- Fitness.py
+++ Fitness.py
@@ -8,34 +8,29 @@ Breaks = int(input("How long do you want your breaks (seconds)?  "))
 Work = int(input("How long do you want your work (seconds)?  "))
 
 #FullBody
-exerciseFullBody = ["Inchworm", "Bear crawl", "Mountain climbers", "Burpee"]
-exerciseJumps = ["Tuck jump", "Star jumps", "Squat jumps"]
-exercisePlank = ["Plank", "Progressive/up-down Plank", "Side plank"]
+exerciseFullBody = ["Inchworm", "Bear crawl", "Mountain climbers", "Burpee","Tuck jump", "Star jumps", "Squat jumps","Plank", "Progressive/up-down Plank", "Side plank"]
 #Legs
-exerciseLegs = ["Wall sit", "Step-up"]
-exerciseLunge = ["Lunge", "Lunge jump", "Reverse Lunge", "Curtsy lunge"]
-exerciseSquat = ["Squats", "Squat Hold", "Squat Pulse"]
+exerciseLegs = ["Wall sit", "Step-up", "Lunge", "Lunge jump", "Reverse Lunge", "Curtsy lunge", "Squats", "Squat Hold", "Squat Pulse"]
 #Chest and back
-excersiseChestBack = ["Superman"]
-excersisePushUp = ["Push-up", "Clap push-up", "Tight push-up", "Rotational push-up"]
+excersiseChestBack = ["Superman", "Push-up", "Clap push-up", "Tight push-up", "Rotational push-up"]
 #Sholders and arms
 excersiseSholderArm = ["Boxing", "Arm circles"]
 #Core
-excersiseCrunches = ["Bycicle crunch", "Crunch/sit-up", "Single leg crunch"]
-excersiseLegRaises = ["Double leg raises", "Single leg raises"]
+excersiseCore = ["Bycicle crunch", "Crunch/sit-up", "Single leg crunch","Double leg raises", "Single leg raises"]
 
-ExcersiseTypeConverter = [exerciseFullBody, exerciseJumps, exercisePlank, exerciseLegs, exerciseLunge, exerciseSquat, excersiseChestBack, excersisePushUp, excersiseSholderArm, excersiseCrunches, excersiseLegRaises]
-excersiseLengthA = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-excersiseLengthB = [3, 2, 2, 1, 3, 2, 0, 3, 1, 2, 1]
+ExcersiseTypeConverter = [exerciseFullBody, exerciseLegs, excersiseChestBack, excersiseSholderArm, excersiseCore]
+##excersiseLengthB = [3, 2, 2, 1, 3, 2, 0, 3, 1, 2, 1]
+
+
 
 
 while excersiseAmount != 0:
     
-    excersiseTypeNumber = random.randint(0,10)
+    excersiseTypeNumber = random.randint(0,4)
 
     excersiseType = ExcersiseTypeConverter[excersiseTypeNumber]
 
-    excersise = excersiseType[random.randint(excersiseLengthA[excersiseTypeNumber],excersiseLengthB[excersiseTypeNumber])]
+    excersise = excersiseType[random.randint(0,(len(excersiseType)-1))]
     if excersises == 0:
         print("Let's start! The first one is the " + excersise +"!")
     else:
